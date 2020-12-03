e8804a784f31706443ef4f90bb2a4fd6675f6b13
dimpangg <72178046+dimpangg@users.noreply.github.com>
2020-12-03 20:49:56 +0700

    Menghubungkan Login dengan Database

--- E41191628_DimasPangestu/web_bootstrap/login_process.php
+++ E41191628_DimasPangestu/web_bootstrap/login_process.php
@@ -1,13 +1,17 @@
 <?php
+include 'koneksi.php';
 $email = $_POST['email'];
 $password = $_POST['password'];
-$email_saya = "dimaspangestu19@gmail.com";
-$password_saya = "pgst123";
-if ((strcasecmp($email_saya, $email) == 0) && (strcasecmp($password_saya, $password) == 0)) {
-    session_start();
-    $_SESSION['email'] = $email;
-    header("location:index.php?pesan=berhasil");
-} else {
-    header("location:login.php?pesan=gagal");
-}
+$querysql = mysqli_query($koneksi, "SELECT * FROM login where email='$email'");
+$data = mysqli_fetch_array($querysql);
+if ($email == $data['email']) {
+    if ($password == $data['password']) {
+        session_start();
+        $_SESSION['email'] = $email;
+        header("location:index.php?pesan=berhasil");
+    } else {
+        header("location:login.php?pesan=gagal");
+        }
+    }
+    
 ?>
