ac625d2c7465b4ec259697adc7681f48cee12038
monokuro49 <07akhmadnur@gmail.com>
2020-12-09 21:48:17 +0700

    Navbar + logo website

---
+++ web-project/scripts/nav.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <style>
+        .brand-icon{
+            height: 30px;
+            float:left;
+            margin-right:10px;
+        }
+        .bg-navbar{
+            background: #FBF1F0;
+        }
+        .navbar-2{
+            background: #FBF1F0;
+        }
+        .a{
+            margin-top: 60px;
+        }
+        nav form button.btn{
+            background: #FBF1F0;
+            color: black;
+            margin-left: 3px;
+        }
+        nav form input.form-control-inline{
+            width: 30em;
+            padding: 5px;
+        }
+        
+    </style>
+    <title>navbar</title>
+  </head>
+  <body>
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+            <div class="container">
+                <a href="#" class="navbar-brand">
+                    <div class="brand">
+                        <img src="../images/hi_valeeqa.png" alt="Brand Icon" class="brand-icon">
+                        <h2>Hi Valeeqa</h2>
+                    </div>
+                </a>
+                <form action="" class="form-inline my-2 my-lg-0" >
+                    <input type="search" class="form-control-inline" name="search">
+                    <button class="btn" type="submit">Search</button>
+                </form> 
+            </div>
+        </nav>
+        <nav class="navbar navbar-expand-lg navbar-light bg-navbar fixed-top a">
+            <div class="container-fluid">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Terbaru</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Yumna Dress</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">Chayra Abaya</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+            
+        
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
