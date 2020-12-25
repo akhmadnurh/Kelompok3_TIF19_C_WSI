8cb81529e140a470a2d19759be411a016b849b68
zavira111 <72178086+zavira111@users.noreply.github.com>
2020-12-25 20:46:39 +0700

    edit-tabungan#3

---
+++ web-project/scripts/tabungan.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Tabungan</title>
+		<style>
+
+		  
+			.box1{
+				width:500px;
+				height:20px;
+				background:#d6c0b3;
+                margin-top: 130px;
+                margin-left: 0%;
+                margin-right: 0%;
+                padding: 12px;   
+			}
+
+           
+            .box2{
+            width: 510px;
+            height: 200px;
+            border: 1px silver;
+            padding: 5px; 
+            margin-bottom: 20px;
+            background: silver;
+        }
+        .box2-img{
+            width: 25%;
+            height: 25px;
+            background: silver;   
+
+        }
+
+        .detail-tab{
+            text-align: left;
+        }
+
+        .box2-total{
+            text-align: right;
+        }
+
+        .box1-detail{
+            width:200px;
+			height:20px;
+			background:#d6c0b3;
+            margin-top: 20px;
+            margin-left: 0%;
+            margin-right: 0%;
+            padding: 12px;
+        }
+
+        .box2-detail{
+            width: 214px;
+            height: 200px;
+            border: 1px silver;
+            padding: 5px; 
+            margin-bottom: 20px;
+            background: silver;
+        }
+
+        .desc{
+            text-align: left;
+            padding: 2px;
+        }
+		</style>
+	</head>
+	<body>
+    <center>
+    <div class="col-sm-9">
+    <div class="box1">TABUNGAN</div>
+                <br>
+                <div class="row">
+                    <div class="col-sm-4">
+                        <div class="box2">
+                            <div class="detail-tab">
+                                <div class="box2-img">Gambar</div>
+                                <p>Yumna Dress</p>
+                                <p>Milo</p>
+                                <p>Price/pcs : Rp 167.000
+                                <p>Jumlah : 1</p>
+                            <div class="box2-total">
+                                <h4>Rp 167.000</h4>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <div class="box2">
+                            <div class="detail-tab">
+                                <div class="box2-img">Gambar</div>
+                                    <p>Chayra Abaya</p>
+                                    <p>Caramel</p>
+                                    <p>Price/pcs : Rp 167.000</p>
+                                    <p>Jumlah : 2
+                                    <br>
+                                <div class="box2-total">
+                                    <h4>Rp 330.000</h4>
+                            </div>
+                        </div>
+                    </div>
+    
+    <div class="col-sm-9">
+    <div class="box1-detail">DETAIL</div>
+                    <div class="col-sm-4">
+                        <div class="box2-detail">
+                            <div class="desc">
+                                <h6><p>Total :</p>
+                                <p>Est Pengiriman :</p>
+                                <p>Grand Total : </p></h6>
+                            </div>
+                        </div>
+                    </div>
+
+            
+    </div>   
+    </center>
+    </body>	        
+</head>
+</html>
