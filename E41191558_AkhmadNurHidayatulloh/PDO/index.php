7d119e9b3139f93fcba83a01976c8701aecc3ff5
monokuro49 <07akhmadnur@gmail.com>
2020-12-28 00:25:59 +0700

    Tugas PHP Data Object

--- E41191558_AkhmadNurHidayatulloh/PDO/index.php
+++ E41191558_AkhmadNurHidayatulloh/PDO/index.php
@@ -3,23 +3,78 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
+    <title>Printer List</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    
+    <!-- CSS -->
+    <style>
+        .container .card{
+            margin-top: 50px;
+            margin-left: 10%;
+            margin-right: 10%;
+        }
+        .col-sm-2, .col-sm-1, .col-sm-4, .col-sm-3{
+            border-style: solid;
+            border-width: 1px;
+        }
+        .card{
+            border-width:0px;
+        }
+    </style>
 </head>
 <body>
-    <?php
-        session_start();
-        if(!isset($_SESSION["status"])){
-            header("Location: login.php");
-        }elseif($_SESSION["status"] == "offline"){
-            header("Location: login.php");
-        }
-        
-    ?>
-    <center>
-        <h1>BERHASIL!!!</h1>
-        <p>Belajar PHP Data Object</p>
-        <br>
-        <p>Klik di <a href="logout.php">sini</a> untuk keluar.</p>
-    </center>
+    <section>
+        <div class="container">
+            <div class="card" id="list">
+                <!-- Field -->
+                <div class="row text-center" >
+                    <div class="col-sm-1">No</div>
+                    <div class="col-sm-3">Nama Merek</div>
+                    <div class="col-sm-2">Warna</div>
+                    <div class="col-sm-2">Jumlah</div>
+                    <div class="col-sm-3">Action</div>
+                </div>
+                    <?php
+
+                        require_once("php/koneksi.php");
+
+                        //Query untuk mengambil semua data dari table printer
+                        $sql = "select * from printer";
+                        $query = $dbh->query($sql);
+                        
+                        //Menampilkan data secara berulang
+                        $no_urut = 1;
+                        while($data = $query->fetch()){
+                            
+                    ?>
+
+                        <!-- Tampilkan data -->
+                            <div class="row text-center">
+                                <div class="col-sm-1"><?php echo $no_urut; ?></div>
+                                <div class="col-sm-3"><p name="merk"><?php echo $data["nama_merek"]; ?></p></div>
+                                <div class="col-sm-2"><?php echo $data["warna"]; ?></div>
+                                <div class="col-sm-2"><?php echo $data["jumlah"] ?></div>
+                                <div class="col-sm-3">
+                                    <button class="btn btn-link" name="edit">
+                                        <a href="php/tambah.php?no=<?= $data["no"]; ?>" >Edit</a>
+                                    </button>
+                                    <button class="btn btn-link" name="edit">
+                                        <a href="php/hapus.php?no=<?= $data["no"]; ?>">Hapus</a>
+                                    </button>
+                                </div>
+                            </div>
+                    <?php $no_urut+=1; }?>
+
+                <!-- Tambah data -->
+                <div class="row" style="margin-top:25px;">
+                    <div class="md-sm-2 text-right">
+                        <a href="php/tambah.php">Tambah Data</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
 </html>
