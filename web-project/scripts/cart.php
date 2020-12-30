b1d2c3be662c58d4e95124f03170ebaf85af977b
dimpangg <72178046+dimpangg@users.noreply.github.com>
2020-12-31 02:11:07 +0700

    UI Keranjang (belum fix)

---
+++ web-project/scripts/cart.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <style>
+        .marg {
+            margin-top: 10rem !important;
+        }
+    </style>
+  </head>
+  <body>
+    <?php include 'nav.php' ?>
+
+    <div class="container mb-5 marg">
+        <div class="row">
+            <!-- Grid Item -->
+            <div class="col-lg-8">
+                
+                <div class="card">
+                    <div class="card-header"><h3>Keranjang</h3></div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-5 col-lg-3 col-xl-3">
+                                <img src="https://raw.githubusercontent.com/monokuro49/Kelompok3_TIF19_C_WSI/master/web-project/images/detail/img4.png" alt="contoh" class="w-100">
+                            </div>
+                            <div class="col-md-5 col-lg-7 col-xl-7">
+                                <b>Chayra Abaya</b><br>
+                                Black <br><br>
+                                <input type="number" value="1" class="form-control w-50 mt-auto">
+                            </div>
+                            <div class="col-md-2 col-lg-2 col-xl-2">
+                                <a href="#" class="btn btn-danger">Hapus</a>
+                            </div>
+                            <div class="ml-auto mr-4">
+                                <b>Rp.150.000</b>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="row">
+                            <div class="col-md-5 col-lg-3 col-xl-3">
+                                <img src="https://raw.githubusercontent.com/monokuro49/Kelompok3_TIF19_C_WSI/master/web-project/images/detail/img4.png" alt="contoh" class="w-100">
+                            </div>
+                            <div class="col-md-5 col-lg-7 col-xl-7">
+                                <b>Chayra Abaya</b><br>
+                                Black <br><br>
+                                <input type="number" value="1" class="form-control w-50 mt-auto">
+                            </div>
+                            <div class="col-md-2 col-lg-2 col-xl-2">
+                                <a href="#" class="btn btn-danger">Hapus</a>
+                            </div>
+                            <div class="ml-auto mr-4">
+                                <b>Rp.150.000</b>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Grid Total -->
+            <div class="col-lg-4">
+                
+                <div class="card">
+                    <div class="card-header"><h3>Total</h3></div>
+                    <div class="card-body">
+                        <table class="table justify-content-between">
+                            <tr>
+                                <td>Total</td>
+                                <td>Rp. 150.000</td>
+                            </tr>
+                        </table>
+                        <center><a href="#" class="btn btn-dark">Checkout</a></center>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include 'footer.php' ?>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
