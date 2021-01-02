b0850d59904f4237b3bc9b68a252047dd4e9d040
monokuro49 <07akhmadnur@gmail.com>
2021-01-02 23:29:02 +0700

    Menambah pagination pada data-produk

--- web-project/admin/dist/data-produk.php
+++ web-project/admin/dist/data-produk.php
@@ -37,13 +37,23 @@
                     // Query
                     $sql = "SELECT * FROM produk";
                     $query = mysqli_query($conn, $sql);
-                    $nomor = 1;
+                    
                     
                     // Pagination
-                    $batas = 2;
+                    $batas = 10;
+                    $halaman = isset($_GET["halaman"]) ? $_GET["halaman"]: 1;
+                    $halaman_awal = $halaman>1 ? ($halaman * $batas) - $batas : 0;// Untuk tiap nomor
                     
+                    $next = $halaman + 1;
+                    $previous = $halaman - 1;
 
+                    $total_data = mysqli_num_rows($query);
+                    $total_halaman = ceil($total_data / $batas);
 
+                    // Query data sesuai halaman
+                    $sql = "SELECT * FROM produk LIMIT $halaman_awal, $batas";
+                    $query = mysqli_query($conn, $sql);
+                    $nomor = $halaman_awal + 1;
                     while($data = mysqli_fetch_array($query)){
                 ?>      
                         <tr>
@@ -65,13 +75,26 @@
             <!-- Pagination -->
             <nav>
                 <ul class="pagination justify-content-end">
-                    <li class="page-item disabled">
-                        <a class="page-link" href="#" tabindex="-1">Previous</a>
-                    </li>
-                    <li class="page-item"><a class="page-link" href="#">1</a></li>
-                    <li class="page-item"><a class="page-link" href="#">2</a></li>
-                    <li class="page-item"><a class="page-link" href="#">3</a></li>
-                    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+                    <?php
+                        if($halaman == 1){
+                            echo "<li class='page-item disabled'><a class='page-link' href='#'>Previous</a></li>";
+                        }else{
+                            echo "<li class='page-item'><a class='page-link' href='data-produk.php?halaman=$previous'>Previous</a></li>";
+                        }
+                    ?>
+                    <?php
+                        
+                        for($i=1; $i<=$total_halaman; $i++){
+                            echo "<li class='page-item'><a class='page-link' href='data-produk.php?halaman=$i'>$i</a></li>";
+                        }
+                    ?>
+                    <?php
+                        if($halaman == $total_halaman){
+                            echo "<li class='page-item disabled'><a class='page-link' href='#'>Next</a></li>";
+                        }else{
+                            echo "<li class='page-item'><a class='page-link' href='data-produk.php?halaman=$next'>Next</a></li>";
+                        }
+                    ?>
                 </ul>
             </nav>
         </div>
