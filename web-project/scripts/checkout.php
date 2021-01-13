17404e568725b6b01e77efa587b838e66d1ee377
dimpangg <72178046+dimpangg@users.noreply.github.com>
2021-01-14 00:52:03 +0700

    Checkout

--- web-project/scripts/checkout.php
+++ web-project/scripts/checkout.php
@@ -13,15 +13,18 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <style>
         .marg {
-            margin-top: 10rem !important;
+            margin-top: 7rem !important;
             margin-bottom: 10rem !important;
+        }
+        .thanks {
+            font-size: 30px;
         }
         .table > tbody > tr > td {
             vertical-align: middle;
         }
         .total {
             font-size: 24px;
-            text-align: right;
+            font-weight: bold;
         }
     </style>
 
@@ -30,114 +33,154 @@
   <body>
     <?php include 'nav.php' ?>
     <div class="container marg">
-        <div class="card">
-            <div class="card-header">
-                <h3>Alamat Pengiriman</h3>
-            </div>
-            <div class="card-body">
-                <div class="table-responsive-sm">
-                    <table class="table table-bordered">
-                        <?php
-                            $id_user = $_SESSION["id_user"];
-                            $sql = "SELECT * FROM user where id_user=$id_user";
-                            $query = mysqli_query($conn, $sql);
-                            $data = mysqli_fetch_array($query);
-                          
-                        ?>
-                        <tr>
-                            <td width="20%">Nama</td>
-                            <td><?php echo $data["nama"];  ?></td>
-                        </tr>
-                        <tr>
-                            <td>Email</td>
-                            <td><?php echo $data["email"];  ?></td>
-                        </tr>
-                        <tr>
-                            <td>No. Hp</td>
-                            <td><?php echo $data["nomor_wa"];  ?></td>
-                        </tr>
-                        <tr>
-                            <td>Alamat</td>
-                            <td><?php echo $data["alamat"];  ?></td>
-                        </tr>
-                    </table>
-                </div>
+        <div class="card mb-4">
+            <div class="card-body thanks">
+                Terimakasih Telah Berbelanja di Hi Valeeqa!
             </div>
         </div>
-
-        <div class="card mt-4">
-            <div class="card-header">
-                <h3>Rincian Pesanan</h3>
-            </div>
-            <div class="card-body">
-                <div class="table-responsive-sm">
-
-                    <?php
-                        $sql = "SELECT * FROM cart inner join produk on cart.id_produk = produk.id_produk inner join gambar on produk.id_produk = gambar.id_produk where id_user=$id_user";
-                        $query2 = mysqli_query($conn, $sql);
-                        $count = mysqli_num_rows($query2);
-                        $total = 0;
-                        if($count < 1){
-                            echo "<h1>Checkout Kosong</h1>";
-                        }else{
-                            $nomor = 1;
-                            $total = 0;
-                            while($data2 = mysqli_fetch_array($query2)){
-                    
-                            
-                    ?>
+        <div class="row">
+            <div class="col-sm-8">
+                <!-- Alamat Pengiriman -->
+                <div class="card">
+                    <div class="card-header">
+                        <h3>Alamat Pengiriman</h3>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive-sm">
                             <table class="table table-bordered">
-                                <thead class="thead-dark">
-                                    <tr>
-                                        <th colspan="3">Pesanan <?php echo $nomor; ?></th>
-                                    </tr>
-                                </thead>
+                                <?php
+                                    $id_user = $_SESSION["id_user"];
+                                    $sql = "SELECT * FROM user where id_user=$id_user";
+                                    $query = mysqli_query($conn, $sql);
+                                    $data = mysqli_fetch_array($query);
+                                
+                                ?>
                                 <tr>
-                                    <td rowspan="4" width="20%"><img src="../<?php echo $data2["lokasi_gambar"]; ?>" class="w-100"></td>
-                                    <td width="20%">Nama Barang</td>
-                                    <td><?php echo $data2["nama_barang"]; ?></td>
+                                    <td width="20%">Nama</td>
+                                    <td><?php echo $data["nama"];  ?></td>
                                 </tr>
                                 <tr>
-                                    <td>Warna</td>
-                                    <td><?php echo $data2["warna"]; ?></td>
+                                    <td>Email</td>
+                                    <td><?php echo $data["email"];  ?></td>
                                 </tr>
                                 <tr>
-                                    <td>Jumlah</td>
-                                    <td><?php echo $data2["jumlah"]; ?></td>
+                                    <td>No. Hp</td>
+                                    <td><?php echo $data["nomor_wa"];  ?></td>
                                 </tr>
                                 <tr>
-                                    <td>Harga</td>
-                                    <td>Rp <?php echo number_format($data2["harga"], 0, "", "."); ?></td>
+                                    <td>Alamat</td>
+                                    <td><?php echo $data["alamat"];  ?></td>
                                 </tr>
                             </table>
-                    <?php
-                                $hitung = $data2["harga"] * $data2["jumlah"];
-                                $total += $hitung;
-                                $nomor++;
-                            }
-                        }
-                                
-                    ?>
-                    
+                        </div>
+                    </div>
+                </div>
+                <!-- Rincian Pesanan -->
+                <div class="card mt-4">
+                    <div class="card-header">
+                        <h3>Rincian Pesanan</h3>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive-sm">
 
-                    <table class="table mt-2">
-                        <tr class="table-secondary total">
-                            <td>Total:</td>
-                            <td>Rp <?php echo number_format($total, 0, "", "."); ?></td>
-                        </tr>
-                    </table>
+                            <?php
+                                $sql = "SELECT * FROM cart inner join produk on cart.id_produk = produk.id_produk inner join gambar on produk.id_produk = gambar.id_produk where id_user=$id_user";
+                                $query2 = mysqli_query($conn, $sql);
+                                $count = mysqli_num_rows($query2);
+                                $total = 0;
+                                if($count < 1){
+                                    echo "<h1>Checkout Kosong</h1>";
+                                }else{
+                                    $nomor = 1;
+                                    $total = 0;
+                                    while($data2 = mysqli_fetch_array($query2)){
+                            
+                                    
+                            ?>
+                                    <table class="table table-bordered">
+                                        <thead class="thead-dark">
+                                            <tr>
+                                                <th colspan="3">Pesanan <?php echo $nomor; ?></th>
+                                            </tr>
+                                        </thead>
+                                        <tr>
+                                            <td rowspan="4" width="20%"><img src="../<?php echo $data2["lokasi_gambar"]; ?>" class="w-100"></td>
+                                            <td width="20%">Nama Barang</td>
+                                            <td><?php echo $data2["nama_barang"]; ?></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Warna</td>
+                                            <td><?php echo $data2["warna"]; ?></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Jumlah</td>
+                                            <td><?php echo $data2["jumlah"]; ?></td>
+                                        </tr>
+                                        <tr>
+                                            <td>Harga</td>
+                                            <td>Rp <?php echo number_format($data2["harga"], 0, "", "."); ?></td>
+                                        </tr>
+                                    </table>
+                            <?php
+                                        $hitung = $data2["harga"] * $data2["jumlah"];
+                                        $total += $hitung;
+                                        $nomor++;
+                                    }
+                                }
+                                        
+                            ?>
+                            <!-- Total -->
+                            <table class="table mt-2">
+                                <tr>
+                                    <td class="total text-left">Total:</td>
+                                    <td class="total text-right">Rp <?php echo number_format($total, 0, "", "."); ?></td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-4">
+                <!-- Pembayaran -->
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <h3>Pembayaran</h3>
+                    </div>
+                    <div class="card-body">
+                        <div class="table-responsive-sm">
+                            <table class="table">
+                                <tr>
+                                    <td>Bank Tujuan</td>
+                                    <td>BNI</td>
+                                </tr>
+                                <tr>
+                                    <td>Atas Nama</td>
+                                    <td>Akhmad Nur Dayat</td>
+                                </tr>
+                                <tr>
+                                    <td>No. Rekening</td>
+                                    <td>07818298938</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <!-- Konfirmasi -->
+                <div class="card mb-4">
+                    <div class="card-header">
+                        <h3>Konfirmasi</h3>
+                    </div>
+                    <div class="card-body">
+                        Konfirmasi pembayaran dengan mengirim foto struk transfer ke whatsapp kami di nomor <b>085784197425</b> atau klik dibawah ini. <br>
+                        <a href="https://wa.me/6285784197425?text=Halo%20Hi%20Valeeqa,%20Saya%20aku%20mengonfirmasi%20pembayaran%20untuk%20Kode%20Transaksi%201" class="btn btn-success w-100 mt-4">Konfirmasi Bukti Transfer</a>
+                    </div>
                 </div>
             </div>
         </div>
+        
 
-        <div class="card mt-4">
-            <div class="card-header">
-                <h3>Pembayaran</h3>
-            </div>
-            <div class="card-body">
-                
-            </div>
-        </div>
+        
     </div>
     <?php include 'footer.php'; ?>
 
